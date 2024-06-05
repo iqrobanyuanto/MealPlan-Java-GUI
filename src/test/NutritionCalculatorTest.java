b6f78db17849e1d89e0b0aa2daf45d7e99251a93
raimess11 <rahma.sakti24@gmail.com>
2024-06-05 21:48:43 +0700

    Update NutritionCalculatorTest.java

--- src/test/NutritionCalculatorTest.java
+++ src/test/NutritionCalculatorTest.java
@@ -1,8 +1,10 @@
+NutritionCalculationTest.java
+
 import org.junit.jupiter.api.Test;
 
 import User.User;
-import UserNutrition;
 import Nutrition.UserNutrition;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class NutritionCalculatorTest {
@@ -10,15 +12,14 @@ public class NutritionCalculatorTest {
   @Test
   public void testNutritionCalculation1() {
 
-    User user = new User("testname1", "testusername1", "testemail1","testpassword1", "attacthelicopter", 16, 152, 48);
+    User user = new User("testname1", "testusername1", "testemail1","testpassword1", "Male", 16, 152, 48);
     UserNutrition calculator = new UserNutrition();
 
     calculator.setNutrition(user);;
 
-    assertEquals(1370, calculator.getCal());
-    assertEquals(105.82176, calculator.getProtein());
-    double expectedFat = (calculator.getCal() * 0.25) / 9;
-    assertEquals(expectedFat, calculator.getFat(), 0.1); // Allow a small delta for floating-point calculations
-    assertEquals(133.5, calculator.getCarbs(), 0.1); // Assuming remaining calories are carbs
+    assertEquals(1702, calculator.getCal());
+    assertEquals(232.0, calculator.getProtein());
+    assertEquals(66.0, calculator.getFat(), 0.1); // Allow a small delta for floating-point calculations
+    assertEquals(43.0, calculator.getCarbs(), 0.1); // Assuming remaining calories are carbs
   }
 }
